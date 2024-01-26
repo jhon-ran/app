d793beb9c35fa2454d85d8f0f28bb84beeb844b1
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-01-26 14:23:42 -0500

    add create & index usuarios section

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,7 +1,60 @@
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/header.php"); ?>
 
-Crear usuarios
+<br/>
+
+<div class="card">
+    <div class="card-header">Datos del usuario</div>
+    <div class="card-body">
+
+        <form action="" method="post" enctype="multipart/form-data">
+            
+            <div class="mb-3">
+                <label for="usuario" class="form-label">Nombre del usuario:</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="usuario"
+                    id="usuario"
+                    aria-describedby="helpId"
+                    placeholder="Nombre del usuario"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input
+                    type="password"
+                    class="form-control"
+                    name="password"
+                    id="password"
+                    aria-describedby="helpId"
+                    placeholder="Escriba contraseña"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="correo" class="form-label">Correo:</label>
+                <input
+                    type="email"
+                    class="form-control"
+                    name="correo"
+                    id="correo"
+                    aria-describedby="helpId"
+                    placeholder="Escriba su correo"
+                />
+            </div>
+            
+            
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+            
+            
+
+        </form>
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
 
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/footer.php"); ?>
--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,7 +1,40 @@
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/header.php"); ?>
 
-Listar usuarios
+<br/>
+
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button" >Agregar usuarios</a>
+    </div>
+    <div class="card-body">
+    <div class="table-responsive-sm">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre del usuario</th>
+                    <th scope="col">Contraseña</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">1</td>
+                    <td>Pedro Páramo</td>
+                    <td>******</td>
+                    <td>pparamo@comala.com</td>
+                    <td>
+                        <input name="btneditar" id="btneditar" class="btn btn-info" type="button" value="Editar"/>
+                        |
+                        <input name="btnborrar" id="btnborrar" class="btn btn-danger" type="button" value="Eliminar"/>
+                     </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </div>
 
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/footer.php"); ?>
