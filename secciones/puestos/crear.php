fd2eb0bdab0746d631b8167b83dd666f854ad6d4
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-01-26 11:55:50 -0500

    create form in puestos add

--- secciones/puestos/crear.php
+++ secciones/puestos/crear.php
@@ -1,7 +1,37 @@
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/header.php"); ?>
 
-Crear puestos
+<br/>
+
+<div class="card">
+    <div class="card-header">Puestos</div>
+    <div class="card-body">
+
+        <form action="" method="post" enctype="multipart/form-data">
+            
+            <div class="mb-3">
+                <label for="nombredelpuesto" class="form-label">Nombre del puesto:</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="nombredelpuesto"
+                    id="nombredelpuesto"
+                    aria-describedby="helpId"
+                    placeholder="Nombre del puesto"
+                />
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+            
+            
+
+        </form>
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
 
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/footer.php"); ?>
