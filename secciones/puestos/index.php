5b8b3d480c3c434ce5715d17266034b4252979ab
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-01-26 21:37:49 -0500

    add reading puestos from db

--- secciones/puestos/index.php
+++ secciones/puestos/index.php
@@ -1,5 +1,16 @@
+<!-- Importar conexión a BD-->
+<?php include("../../bd.php"); 
+
+$sentencia = $conexion->prepare("SELECT * FROM tbl_puestos");
+$sentencia->execute();
+$lista_tbl_puestos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+//Para probar que se esté leyendo todos los datos de la tabla, descomentar
+//print_r($lista_tbl_puestos);
+?>
+
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
-<?php include("../../templates/header.php"); ?>
+<?php include("../../templates/header.php");?>
 
 <br/>
 
@@ -18,15 +29,21 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="">
-                    <td scope="row">1</td>
-                    <td>Taquero</td>
+
+            <?php foreach($lista_tbl_puestos as $registro){ ?>
+                
+                <td scope="row"><?php echo $registro['id']?></td>
+                    <td><?php echo $registro['nombredelpuesto']?></td>
                     <td>
                         <input name="btneditar" id="btneditar" class="btn btn-info" type="button" value="Editar"/>
                         |
                         <input name="btnborrar" id="btnborrar" class="btn btn-danger" type="button" value="Eliminar"/>
                      </td>
                 </tr>
+
+                <?php }?>
+                <tr class="">
+
             </tbody>
         </table>
     </div>
