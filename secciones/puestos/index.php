04e6c539038c9ba94d3e6411211fe6599678258b
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-01-28 15:10:30 -0500

    add delete functionality for puestos

--- secciones/puestos/index.php
+++ secciones/puestos/index.php
@@ -1,13 +1,33 @@
 <!-- Importar conexión a BD-->
 <?php include("../../bd.php"); 
 
+//******Inicia código para eliminar registro******
+//Para recolectar información del url con el botón "eliminar" método GET
+if(isset($_GET['txtID'])){
+
+    //Si esta variable existe, se asigna ese valor, de lo contrario se queda
+    $txtID = (isset($_GET['txtID']))?$_GET['txtID']:$_GET['txtID'];
+    //Se prepara sentencia para borrar dato seleccionado (id)
+    $sentencia = $conexion->prepare("DELETE FROM tbl_puestos WHERE id=:id");
+    //Asignar los valores que vienen del método GET (id seleccionado por params)
+    $sentencia->bindParam(":id",$txtID);
+    //Se ejecuta la sentencia con el valor asignado para borrar
+    $sentencia->execute();
+    //Redirecionar después de eliminar a la lista de puestos
+    header("Location:index.php");
+}
+//******Termina código para eliminar registro******
+
+//******Inicia código para mostrar todos los registros******
+//Se prepara sentencia para seleccionar todos los datos 
 $sentencia = $conexion->prepare("SELECT * FROM tbl_puestos");
 $sentencia->execute();
 $lista_tbl_puestos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
-
 //Para probar que se esté leyendo todos los datos de la tabla, descomentar
 //print_r($lista_tbl_puestos);
+//******Termina código para mostrar todos los registros******
 ?>
+
 
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/header.php");?>
@@ -36,8 +56,10 @@ $lista_tbl_puestos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
                     <td><?php echo $registro['nombredelpuesto']?></td>
                     <td>
                         <input name="btneditar" id="btneditar" class="btn btn-info" type="button" value="Editar"/>
-                        |
-                        <input name="btnborrar" id="btnborrar" class="btn btn-danger" type="button" value="Eliminar"/>
+                        | 
+                        <!--Envia el id através de la url-->
+                        <a class="btn btn-danger" href="index.php?txtID=<?php echo $registro['id']?>" role="button">Eliminar</a>
+                        
                      </td>
                 </tr>
 
