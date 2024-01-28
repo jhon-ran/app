267c42a5cf7587c1024ca5e9ea4654d68d520e09
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-01-27 21:24:21 -0500

    create puestos section

--- secciones/puestos/crear.php
+++ secciones/puestos/crear.php
@@ -1,3 +1,23 @@
+<!-- Importar conexión a BD-->
+<?php include("../../bd.php"); 
+
+if($_POST){
+    print_r($_POST);
+
+    //Validación: que exista la información enviada, lo vamos a igualar a ese valor,
+    //de lo contratrio lo deja en blanco
+    $nombredelpuesto = (isset($_POST["nombredelpuesto"])? $_POST["nombredelpuesto"]:"");
+    //Preparar la inseción de los datos enviados por POST
+    $sentencia = $conexion->prepare("INSERT INTO tbl_puestos(id,nombredelpuesto) VALUES (null, :nombredelpuesto)" );
+    //Asignar los valores que vienen del formulario (POST)
+    $sentencia->bindParam(":nombredelpuesto",$nombredelpuesto);
+    //Se ejecuta la sentencia con los valores de param asignados
+    $sentencia->execute();
+    //Redirecionar a la lista de puestos
+    header("Location:index.php");
+}
+?>
+
 <!-- ../../ sube 2 niveles para poder acceder al folder de templates-->
 <?php include("../../templates/header.php"); ?>
 
